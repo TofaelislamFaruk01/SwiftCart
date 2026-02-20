c8131da9a7cb346df8f04b2898d85588f8d47cb8
Tofael Islam Faruk <tofaelfaruk@gmail.com>
2026-02-20 16:31:09 +0600

    fetch all products and by category and shou in the ui

--- JS/script1.js
+++ JS/script1.js
@@ -1,23 +1,97 @@
+const categoriesContainer = document.getElementById("categories");
+//console.log(categoriesContainer);
+const productsContainer = document.getElementById("products");
+//console.log(productsContainer);
 
+const categories = fetch("https://fakestoreapi.com/products/categories")
+  .then((res) => res.json())
+  .then((data) => fetchCategories(data))
+  .catch((err) => console.log("Failed to fetch categories:", err));
 
-const categoriesContainer = document.getElementById('categories');
-console.log(categoriesContainer);
+//console.log(categories);
 
-const categories = fetch('https://fakestoreapi.com/products/categories')
-.then(res=>res.json())
-.then(data=> fetchCategories(data))
-.catch(err=> console.log("Failed to fetch categories:", err));
-
-console.log(categories);
+const allProducts = fetch("https://fakestoreapi.com/products")
+  .then((res) => res.json())
+  .then((data) => renderProducts(data))
+  .catch((err) => console.log("Failed to fetch products:", err));
 
 async function fetchCategories(categories) {
-    for(category of categories){
-        console.log(category);
-        const Button = document.createElement('button');
-        Button.classList.add('btn','btn-md','btn-outline','font-medium','border','border-gray-300','rounded-2xl','px-5');
-        Button.innerText= category;
-        console.log(Button)
-        categoriesContainer.appendChild(Button);
+  for (const category of categories) {
+    console.log(category);
+    const Button = document.createElement("button");
+    Button.classList.add(
+      "btn",
+      "btn-md",
+      "btn-outline",
+      "font-medium",
+      "border",
+      "border-gray-300",
+      "rounded-2xl",
+      "px-5",
+    );
+    Button.innerText = category;
+    Button.addEventListener("click", () => {
+      fetchProductsByCategories(category);
+    });
+    console.log(Button);
+    categoriesContainer.appendChild(Button);
+  }
+}
 
-    }
+async function fetchProductsByCategories(category) {
+  //console.log(category);
+
+  const products = await fetch(
+    `https://fakestoreapi.com/products/category/${category}`,
+  )
+    .then((res) => res.json())
+    .then((data) => renderProducts(data))
+    .catch((err) =>
+      console.log(`Failed to fetch products for category ${category}:`, err),
+    );
+}
+
+async function renderProducts(products) {
+  productsContainer.innerHTML = products
+    .map(
+      (product) => `
+      <div class="card bg-white w-full shadow-sm rounded-lg overflow-hidden flex flex-col">
+        <figure class="bg-gray-300 p-8 flex justify-center items-center flex-grow">
+          <img src="${product.image}" alt="${product.title}" class="max-h-60 min-h-60 w-auto object-contain" />
+        </figure>
+        <div class="card-body p-4">
+          <div class="flex justify-between items-center mb-2">
+            <div class="text-xs bg-indigo-100 text-indigo-600 rounded-2xl px-3 py-0.5 font-semibold">
+              ${product.category}
+            </div>
+            <div class="flex items-center text-yellow-500 text-sm font-semibold space-x-1">
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 fill-current" viewBox="0 0 20 20">
+                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.286 3.97a1 1 0 00.95.69h4.184c.969 0 1.371 1.24.588 1.81l-3.39 2.462a1 1 0 00-.364 1.118l1.287 3.97c.3.921-.755 1.688-1.54 1.118l-3.39-2.462a1 1 0 00-1.176 0l-3.39 2.462c-.784.57-1.838-.197-1.539-1.118l1.286-3.97a1 1 0 00-.364-1.118L2.04 9.397c-.783-.57-.38-1.81.588-1.81h4.184a1 1 0 00.95-.69l+1.286-3.97z"/>
+              </svg>
+              <span>${product.rating.rate} (${product.rating.count})</span> 
+            </div>
+          </div>
+            <h2 class="font-semibold text-lg line-clamp-1 mb-1">${product.title}</h2>
+            <p class="font-bold text-lg mb-4">$${product.price}</p>
+            <div class="flex space-x-3">
+                  <button
+                    class="btn btn-outline btn-sm flex-1 gap-2 justify-center border-slate-300 shadow-sm rounded-lg"
+                    type="button"
+                  >
+                    <i class="fas fa-eye text-sm"></i>
+                    Details
+                  </button>
+                  <button
+                    class="btn btn-primary btn-sm flex-1 gap-2 justify-center rounded-lg"
+                    type="button"
+                  >
+                    <i class="fas fa-shopping-cart text-sm"></i>
+                    Add
+                  </button>
+            </div>
+        </div>
+      </div>
+    `,
+    )
+    .join("");
 }
